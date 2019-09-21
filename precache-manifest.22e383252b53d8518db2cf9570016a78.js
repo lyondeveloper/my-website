db9698a7d716fa193c4de8b9b7d3d6a265589edc
Jesus Rincon <contactojesuserincon@gmail.com>
2019-09-21 14:02:00 -0300

    Updates

---
+++ precache-manifest.22e383252b53d8518db2cf9570016a78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208c3483d5ec941d3f09936025bd690b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f5f3bb83a009109b1e3",
+    "url": "/static/css/main.25f4e1e5.chunk.css"
+  },
+  {
+    "revision": "8b4a2a70912928ed7ba7",
+    "url": "/static/js/2.20b16971.chunk.js"
+  },
+  {
+    "revision": "0f5f3bb83a009109b1e3",
+    "url": "/static/js/main.b0412690.chunk.js"
+  },
+  {
+    "revision": "ab25265aa4e361612e4a",
+    "url": "/static/js/runtime-main.b1714db6.js"
+  },
+  {
+    "revision": "4d2ef3199405c10b8a244e2c8ffbf76a",
+    "url": "/static/media/Lyon-Setup-min.4d2ef319.JPG"
+  },
+  {
+    "revision": "00051dbb5b9c7a7f4dfdf04f1b633445",
+    "url": "/static/media/background.00051dbb.jpg"
+  }
+]);
